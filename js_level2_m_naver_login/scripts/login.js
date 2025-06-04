9c623b3b759a97f248f1b0638068def7fac0ff21
surim95 <cheasulim2731@gmail.com>
2025-06-04 10:19:24 +0900

    로그인 에러 조건1 완성

--- js_level2_m_naver_login/scripts/login.js
+++ js_level2_m_naver_login/scripts/login.js
@@ -6,7 +6,7 @@
 const langSelectBtn = document.querySelector ('#lang_select');
 const langGroupOpen = document.querySelector ('.lang_group .lang_open');
 const langSelectChange = document.querySelectorAll ('.lang_group .lang_open > a');
-console.log (langGroupOpen, langSelectBtn);
+console.log (langGroupOpen, langSelectBtn, langSelectChange);
 //langGroup 안보이게 (기본 설정)
 langGroupOpen.style.display = 'none'
 let openif = false; //초기 거짓(숨겨진) 상태
@@ -34,3 +34,46 @@ langSelectBtn.addEventListener('click',()=> {
             langSelectBtn.textContent = langArray.textContent;
         })
 })
+//네이버 로그인 에러 메세지 알고리즘
+/* 
+조건1) 아이디와 비밀번호를 모두 입력하지 않고 로그인 클릭 시
+조건1 결과) 아이디 또는 전화번호를 입력해 주세요.
+조건2) 아이디 작성O, 비밀번호 작성X 로그인 클릭 시
+조건2 결과) 비밀번호를 입력해 주세요.
+조건3) 아이디 작성O, 비밀번호 작성O 로그인 클릭 시
+조건3 결과) 한번 틀릴 시: 아이디(로그인 전화번호, 로그인 전용 아이디) 또는 비밀번호가 잘못 되었습니다. 아이디와 비밀번호를 정확히 입력해 주세요.
+조건4) 조건3 상황에서 로그인 실패가 3회 이상일 때 
+조건4 결과) 영수증 퀴즈와 ' 아이디(로그인 전화번호, 로그인 전용 아이디) 또는 비밀번호가 잘못 되었습니다. 아이디와 비밀번호를 정확히 입력해 주세요.' 메세지 출력
+조건5) 조건4 상황에서 페이지 새로고침할 시
+조건5 결과) 다시 로그인해 주세요.
+*/
+
+//조건1) 아이디와 비밀번호를 모두 입력하지 않고 로그인 클릭 시
+//조건1 결과) 아이디 또는 전화번호를 입력해 주세요.
+const errorMsg = document.querySelector ('#login_frm .error_msg');
+const loginBtn = document.querySelector ('#login_btn');
+const userId = document.querySelector ('#login_frm .id_pw_wrap input[name*=id]');
+const userPw = document.querySelector ('#login_frm .id_pw_wrap input[name*=pw]');
+console.log(errorMsg, loginBtn, userId, userPw);
+errorMsg.style.display = 'none'
+loginBtn.addEventListener('click',()=>{
+    errorMsg.style.display = 'block'
+    /* if (userId.value == '' && userPw.value == '') {
+        errorMsg.textContent = '아이디 또는 전화번호를 입력해 주세요.';
+    }
+    //조건2) 아이디 작성O, 비밀번호 작성X 로그인 클릭 시
+    //조건2 결과) 비밀번호를 입력해 주세요.
+    else if (userId.value != '' && userPw.value == '') {
+        errorMsg.textContent = '비밀번호를 입력해 주세요.';
+    }
+    //조건3) 아이디 작성O, 비밀번호 작성O 로그인 클릭 시
+    //조건3 결과) 한번 틀릴 시: 아이디(로그인 전화번호, 로그인 전용 아이디) 또는 비밀번호가 잘못 되었습니다. 아이디와 비밀번호를 정확히 입력해 주세요.
+    else if (userId.value != '' && userPw.value != '') {
+        errorMsg.textContent = '아이디(로그인 전화번호, 로그인 전용 아이디) 또는 비밀번호가 잘못 되었습니다. 아이디와 비밀번호를 정확히 입력해 주세요.';
+    } */
+    //삼항조건 활용 조건1
+    //조건식 ? 조건식 참 결과 : 조건식 거짓 결과
+    //조건결과 대입 변수 = 조건식 ? 조건식 참 결과 : 조건식 거짓 결과
+    errorMsg.textContent = userId.value == '' && userPw.value == '' ? '아이디 또는 전화번호를 입력해 주세요.' : ''
+    //리액트는 if 보다 삼항조건을 많이 사용.
+});
